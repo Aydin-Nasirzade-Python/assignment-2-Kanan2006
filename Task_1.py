eea5e61ba051179227bd3210eb6e005fc85533e7
Kanan2006 <160099988+Kanan2006@users.noreply.github.com>
2024-04-07 23:40:02 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,7 +1,6 @@
 #import libraries here
 
 def main():
-  
 symbol = input("Enter a letter of the alphabet: ")
 
 if symbol == "a" or symbol == "e" or symbol == "i" or symbol == "o" or symbol == "u":
@@ -10,11 +9,6 @@ elif symbol == "y":
     print("Sometimes it is a vowel, and sometimes it is a consonant!")
 else:
     print("Entered alphabet is a consonant!")
-  
-  
-  
-
-  
   pass
 if __name__ == "__main__":
   main()
