36af0dbf39ccaffd2933b5cfccd80aab4a3b5259
Kanan2006 <160099988+Kanan2006@users.noreply.github.com>
2024-04-07 23:21:39 +0400

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -1,9 +1,36 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+   year = int(input("Enter the year [ex. 2021]: "))
+    burcheyvani = ""
+    if year % 12 == 0:
+             burcheyvani = "Monkey"
+    elif year % 12 == 1:
+             burcheyvani = "Rooster"
+    elif year % 12 == 2:
+             burcheyvani = "Dog"
+    elif year % 12 == 3:
+             burcheyvani = "Pig"
+    elif year % 12 == 4:
+             burcheyvani = "Rat"
+    elif year % 12 == 5:
+             burcheyvani = "Ox"
+    elif year % 12 == 6:
+             burcheyvani = "Tiger"
+    elif year % 12 == 7:
+             burcheyvani = "Hare"
+    elif year % 12 == 8:
+             burcheyvani = "Dragon"
+    elif year % 12 == 9:
+             burcheyvani = "Snake"
+    elif year % 12 == 10:
+             burcheyvani = "Horse"
+    elif year % 12 == 11:
+             burcheyvani = "Sheep"
+    if year >= 0:
+        print("%d is the year of the %s" %(year, animal))
+    else:
+        print("Invalid year!")
   pass
 
 if __name__ == "__main__":
