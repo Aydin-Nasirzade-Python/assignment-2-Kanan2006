8482b097c2ce9788ead65fe5e1d319d44abbd519
Kanan2006 <160099988+Kanan2006@users.noreply.github.com>
2024-04-07 23:37:26 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -2,12 +2,13 @@
 
 def main():
   
-  x=input(" Enter a letter of the alphabet: ")
-  if x=="a" or x=="e" or x=="i" or x=="o" or x=="u":
+symbol = input("Enter a letter of the alphabet: ")
+
+if symbol == "a" or symbol == "e" or symbol == "i" or symbol == "o" or symbol == "u":
     print("Entered alphabet is a vowel!")
-  elif x=="y":
+elif symbol == "y":
     print("Sometimes it is a vowel, and sometimes it is a consonant!")
-  else:
+else:
     print("Entered alphabet is a consonant!")
   
   
