f08691eb126a1b04c08561d7d0c162a9339cdc42
Kanan2006 <160099988+Kanan2006@users.noreply.github.com>
2024-04-07 23:45:51 +0400

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -1,22 +1,35 @@
 #import libraries here
 
 def main():
-  number=int(input("Enter the wavelength in nm: "))
-  if 380<=number and number<=750:
-    if 380<=number and number<450:
-        print("The relevant color is Violet")
-    elif 450<=number and number<495:
-        print("The relevant color is Blue")
-    elif 495<=number and number<570:
-        print("The relevant color is Green")
-    elif 570<=number and number<590:
-        print("The relevant color is Yellow")
-    elif 590<=number and number<620:
-        print("The relevant color is Orange")
-    else:
-        print("The relevant color is Red")
-  else:
-    print("Invalid input!")
+  year = int(input("Enter the year [ex. 2021]: "))
 
+  if year < 0:
+      print("Invalid year!")
+  else:
+      if (year - 2000) % 12 == 0:
+          print(f"{year} is the year of the Dragon")
+      elif (year - 2000) % 12 == 1:
+          print(f"{year} is the year of the Snake")
+      elif (year - 2000) % 12 == 2:
+          print(f"{year} is the year of the Horse")
+      elif (year - 2000) % 12 == 3:
+          print(f"{year} is the year of the Sheep")
+      elif (year - 2000) % 12 == 4:
+          print(f"{year} is the year of the Monkey")
+      elif (year - 2000) % 12 == 5:
+          print(f"{year} is the year of the Rooster")
+      elif (year - 2000) % 12 == 6:
+          print(f"{year} is the year of the Dog")
+      elif (year - 2000) % 12 == 7:
+          print(f"{year} is the year of the Pig")
+      elif (year - 2000) % 12 == 8:
+          print(f"{year} is the year of the Rat")
+      elif (year - 2000) % 12 == 9:
+          print(f"{year} is the year of the Ox")
+      elif (year - 2000) % 12 == 10:
+          print(f"{year} is the year of the Tiger")
+      else:
+          print(f"{year} is the year of the Hare")
+  pass
 if __name__ == "__main__":
   main()
