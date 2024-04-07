5bceb106c200f2fd5d9514f9bc70add0b9085760
Kanan2006 <160099988+Kanan2006@users.noreply.github.com>
2024-04-07 23:13:00 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,9 +1,16 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  month = input("Enter name of the month [ex. June]: ")
+  day = int(input("Enter the day [ex. 5] : "))
+  if month == "March" and (day>=20 and  day<=31) or month == "April" and day>=1 and day<=31 or month=="May" and day>=1 and day<=31 or (month=="June" and (day>=1 and day<21)):
+    print(f"{month} {day} is in Spring")
+  elif month == "June" and (day>=21 and day<=30) or month=="July" and day>=1 and day<=31 or month=="August" and day>=1 and day<=31 or (month=="September" and (day>=1 and day<22)):
+    print(f"{month} {day} is in Summer")
+  elif month=="September" and (day>=22 and day<=31) or month=="October" and day>=1 and day<=31 or month=="November" and day>=1 and day<=31 or month=="December" and day>=1 and day<21:
+    print(f"{month} {day} is in Fall")
+  else:
+    print(f"{month} {day} is in Winter")
   pass
 
 if __name__ == "__main__":
